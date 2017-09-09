07ab7f594b1a2042e75b76b4f42c6b813292b45f
luigisaetta <luigi.saetta@gmail.com>
2017-09-09 17:01:18 +0200

    some mods to raceSimulator

--- raceSimulator.py
+++ raceSimulator.py
@@ -8,10 +8,6 @@ import time
 import sys
 import paho.mqtt.client as mqtt
 
-
-# Custom class for MQTT communication
-from Device import Device
-
 #
 # functions definition
 #
@@ -38,19 +34,30 @@ def on_publish(mqttc, obj, mid):
 def on_log(mqttc, obj, level, string):
     print(string)
 
+def on_disconnect():
+    print("MQTT connection closed")
+
+def wait_for_conn_ok():
+    while (connOk != True):
+        print("Waiting for MQTT connection...")
+        time.sleep(1)
 #
 # Configuration for MQTT protocol
 #
 # the broker, in my Ravello env
 host = "153.92.32.90"
 port = 1883
+# Topics
 # topic name for speed msg
 topic = "speed/msg"
 # topic name for alert msg
-topicAlert ="alert/msg"
+topicAlert = "alert/msg"
+#topic name for lap msgs
+topicLap = "lap/msg"
+
 timeout = 60
 client = "Saetta"
-# guaranteed delivery (at least once)
+# QoS: guaranteed delivery (at least once)
 myQos = 1
 
 #
@@ -69,75 +76,74 @@ sleepTime = 0.2
 print("*******************")
 print("Starting simulation....")
 #
-# reading file name from command line
+# reading file name from command line args
 #
 fName = sys.argv[1]
 print("")
 print("File name: ", fName)
 
 # set MQTT client ID
-mqttc = mqtt.Client(client)
+mqttClient = mqtt.Client(client)
 # note that the client id must be unique on the broker
 
 # MQTT callbacks registration
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
+mqttClient.on_message = on_message
+mqttClient.on_connect = on_connect
+mqttClient.on_publish = on_publish
+mqttClient.on_disconnect = on_disconnect
 
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
 
-mqttc.connect(host, port, timeout)
+mqttClient.connect(host, port, timeout)
 
-mqttc.loop_start()
+mqttClient.loop_start()
 
 connOk = False
 nMsgs = 1
 
-#
-# Test Device Class
-# not yet used
-#
-# myDevice = Device()
-
-# myDevice.publish("Hello")
-
-
+# open the input file and then... read, pulish loop
 with open(fName) as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=';')
+    readerCSV = csv.reader(csvfile, delimiter=';')
     
     # wait for MQTT connection OK
     # connOk is set by the callback on_connect
-    while (connOk != True):
-        print("Waiting for MQTT connection...")
-        time.sleep(1)
+    # (at this point should be connected)
+    wait_for_conn_ok()
 
     # read line by line...    
-    for row in readCSV:
+    for row in readerCSV:
         tempo = row[0]
         tipoMsg = row[1]
         msg = row[3]
         # convert in JSON
         msgJson = json.loads(msg)
         
-        # remove data not useful from JSON
+        # remove data not useful for Console from JSON
         del msgJson["carId"]
         del msgJson["deviceId"]
         del msgJson["demozone"]
-        
+        del msgJson["raceId"]
+        del msgJson["raceStatus"]
+        del msgJson["dateTime"]
+
         if ((tipoMsg == "data") and ("lapTime" not in msgJson)):
             # speed msgs
-        
             # send !
-            mqttc.publish(topic, msg, qos=myQos)
+            mqttClient.publish(topic, msg, qos=myQos)
             print(msgJson)
 
         elif (tipoMsg == "alert"):
              # offtrack msgs
-             mqttc.publish(topicAlert, msg, qos=myQos)               
+             mqttClient.publish(topicAlert, msg, qos=myQos)           
              print(msgJson)
+        
+        elif ((tipoMsg == "data") and ("lapTime" in msgJson)):
+             # lap msgs
+             mqttClient.publish(topicLap, msg, qos=myQos)
+             print("LAPMSG *** ", msgJson)
 
-        # for now we don't process lap messages
+      
         
         # sleep before next iteration
         nMsgs = nMsgs + 1
@@ -148,4 +154,7 @@ with open(fName) as csvfile:
 print()
 print("*******************")
 print("End of simulation...")
-print("Num. of msgs processed", nMsgs)
+print("Num. of msgs processed:", nMsgs)
+
+# close
+mqttClient.disconnect()
