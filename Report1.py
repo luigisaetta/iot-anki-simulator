056da2506bb98c0d8b6fb6645bc5c5c7ca9c8f39
luigisaetta <luigi.saetta@gmail.com>
2017-09-11 14:54:55 +0200

    modified Report1.py

--- Report1.py
+++ Report1.py
@@ -1,5 +1,7 @@
 """
-#
+# This module contains the code to produce a report
+# against the laps table in MySQL DB
+# the table contains for each anki car the time needed to complete a lap
 #
 """
 import mysql.connector
@@ -19,13 +21,12 @@ cursor.execute(query)
 
 # Heading
 print()
-print("******************************")
-print("{:6} {:10} {:4} {:12}".format("raceId", "carName", "lap", "lapTime"))
-print("******************************")
-print()
+print("|*************************************|")
+print("|{:6} - {:10} - {:4} - {:8}|".format("raceId", "carName", "lap", "lapTime"))
+print("|*************************************|")
 
 for (raceId, carName, lap, lapTime) in cursor:
-    print("{:6} {:10} {:4} {:12}".format(raceId, carName, str(lap), str(lapTime)))
+    print("|{:6} | {:10} | {:4} | {:8}|".format(raceId, carName, str(lap), str(lapTime)))
 
 print()
 
